2d729bd5bd3ddf1da208cc15e796f71f38a68d10
basheer mulla <Moranp98@gmail.com>
2023-11-04 23:11:44 +0200

    Complete- <Case 1: App starts> and <Case 3: Show other data>

--- ReactMidProject/src/App.jsx
+++ ReactMidProject/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap-v5'
+import { Button } from 'react-bootstrap-v5';
+import { getAll } from './Rest_API/utils';
 import HomePageComp from './pages/HomePage/HomePage';
 import AddNewUserComp from './pages/AddNewUser/AddNewUser';
 import OtherDataComp from './pages/OtherData/OtherData';
@@ -9,9 +10,16 @@ import UpdateUserComp from './pages/UpdateUser/UpdateUser';
 import DeleteUserComp from './pages/DeleteUser/DeleteUser';
 import AddTodoComp from './pages/AddTodo/AddTodo';
 import AddPostComp from './pages/AddPost/AddPost';
+import UserCardComp from './components/UserCard/UserCard';
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const navigate = useNavigate();
 
@@ -36,9 +44,64 @@ function App() {
     }
   }
 
+  const checkCompletedTodos = (userId) => {
+    const todosUser = todos.filter((todo) => todo.userId === userId);
+    const notYet = todosUser.find((todo) => !todo.completed);
+    return !notYet
+
+  }
+
+  // <-------------- useEffect USERS -------------->
+  useEffect(() => {
+    // // storing input name
+    // localStorage.setItem("name", JSON.stringify(name));
+
+    // Get All Users =>>  <<--- Rest API called utils.js --->>
+    const getAllUsers = async () => {
+      const { data } = await getAll(USERS_URL);
+      console.log(data);
+
+      // Update state
+      setUsers(data);
+    }
+
+    // Invoke the async function
+    getAllUsers();
+  }, []);
+
+  // <-------------- useEffect TODOS -------------->
+  useEffect(() => {
+    // Get All Todos =>>  <<--- Rest API called utils.js --->>
+    const getAllTodos = async () => {
+      const { data } = await getAll(`${TODOS_URL}`);
+      console.log(data);
+
+      // Update state
+      setTodos(data);
+    }
+
+    // Invoke the async function
+    getAllTodos();
+  }, []);
+
+  // <-------------- useEffect POSTS -------------->
+  useEffect(() => {
+    // Get All Posts =>>  <<--- Rest API called utils.js --->>
+    const getAllPosts = async () => {
+      const { data } = await getAll(`${POSTS_URL}`);
+      console.log(data);
+
+      // Update state
+      setPosts(data);
+    }
+
+    // Invoke the async function
+    getAllPosts();
+  }, []);
+
   return (
     <>
-      <div className="p-5 text-white text-center" style={{ backgroundColor: '#3083cb' }}>
+      <div className="p-5 text-white text-center bg-dark" /*style={{ backgroundColor: '#3083cb' }}*/>
         <h1>My React Application</h1>
         <p>This is the My Mid Project of React course!</p>
       </div>
@@ -54,29 +117,13 @@ function App() {
       </nav>
       <div className="container mt-5">
         <div className="row">
-          <div className="col-sm-7">
+          <div className="col-sm-7 border border-dark" style={{ borderRadius: '64px' }}>
             <h2>My Users</h2>
-            <div className='card'>
-              <h5>ID [Number]</h5>
-              <h5> Name [String]</h5>
-              <h5> Email [String]</h5>
-              <div className='row'>
-                <div className='col-sm-4'>
-                  <Button variant="primary" onClick={handleNavigate}> Other Data </Button><br /><br />
-                </div>
-                <div className='col-sm-2'>
-                  <Button variant="primary" onClick={handleNavigate}> Update </Button><br /><br />
-                </div>
-                <div className='col-sm-2'>
-                  <Button variant="primary" onClick={handleNavigate}> Delete </Button><br /><br />
-                </div>
-              </div>
-              <div>
-                <h5> Street [String]</h5>
-                <h5> City [String]</h5>
-                <h5>Zip Code [Number]</h5>
-              </div>
-            </div>
+            {
+              users.map((user) => {
+                return <UserCardComp userData={user} key={user.id} callbackNavigate={handleNavigate} isUnCompleted={checkCompletedTodos(user.id)} />
+              })
+            }
           </div>
           <Routes>
             <Route path='/' element={<HomePageComp />} />
@@ -91,7 +138,7 @@ function App() {
         </div>
       </div>
 
-      <div className="mt-5 p-4 text-white text-center" style={{ backgroundColor: '#3083cb' }}>
+      <div className="mt-5 p-4 text-white text-center bg-dark" /*style={{ backgroundColor: '#3083cb' }}*/>
         <h4>Footer</h4>
       </div>
     </>
--- ReactMidProject/src/components/UserCard/UserCard.jsx
+++ ReactMidProject/src/components/UserCard/UserCard.jsx
@@ -1,56 +1,86 @@
 
-import { useState } from 'react';
-import { Form, Button, Row } from 'react-bootstrap'
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap-v5';
 import './UserCard.css'
-import { useNavigate } from 'react-router-dom';
 
-function UserCardComp() {
-    // const [fName, setFName] = useState('');
-    // const [lName, setLName] = useState('');
+function UserCardComp({ userData, callbackNavigate, isUnCompleted }) {
+    const [user, setUser] = useState({ id: '', name: '', email: '', street: '', city: '', zipcode: '' });
+    const [showOtherData, setShowOtherData] = useState(false);
+    const [isCompleted, setIsCompleted] = useState(isUnCompleted);
 
-    // const navigate = useNavigate();
-
-    // const handleSubmit = async (e) => {
-
-    //     e.preventDefault();
-
-    //     const form = e.currentTarget;
-    //     console.log(form.checkValidity());
-    //     if (form.checkValidity() === false) {
-    //         e.preventDefault();
-    //         e.stopPropagation();
-    //     }
-
-    //     const obj = { state: { firstName: fName, lastName: lName } }
-    //     let path = `cityPath`;
-
-    //     navigate(`/${path}`, obj);
-    // }
+    useEffect(() => {
+        setUser(userData);
+    }, [])
 
     return (
         <>
-            <div>
-                <h3> User Card </h3>
-                {/* <Row className="mb-3">
-                    <Form onSubmit={handleSubmit}>
-                        <div className='card div_hieght'>
-                            <Form.Group className="mb-3" onChange={(e) => setFName(e.target.value)} controlId="exampleForm.ControlInput1">
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control required type="text" placeholder="First Name" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" onChange={(e) => setLName(e.target.value)} controlId="exampleForm.ControlInput1">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control required type="text" placeholder="Last Name" />
-                            </Form.Group>
-                            <br />
+            <div className='card' style={!isCompleted ? { borderColor: 'red' } : { borderColor: 'green' }}>
+                <div className='row'>
+                    <div className='col-sm-2'>
+                        <label className='displayInLine_label'> ID: {user.id} </label>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-sm-2'>
+                        <label className='displayInLine_label'> Name: </label>
+                    </div>
+                    <div className='col-sm-6'>
+                        <input className="form-control-static" type="text" value={user.name} readOnly />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-sm-2'>
+                        <label className='displayInLine_label'> Email: </label>
+                    </div>
+                    <div className='col-sm-6'>
+                        <input className="form-control-static" type="text" value={user.email} readOnly />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-sm-4'>
+                        <Button variant="secondary" onMouseOver={(e) => setShowOtherData(true)}> Other Data </Button>
+                    </div>
+                </div>
+                <br />
+                <div className='otherData_div' hidden={!showOtherData} onClick={() => setShowOtherData(false)}>
+                    <div className='row'>
+                        <div className='col-sm-2'>
+                            <label className='displayInLine_label'> Street: </label>
                         </div>
-                        <br />
-                        <Button className='btn btn-secondary' onClick={() => navigate(-1)}><b>{'<- '} Back </b></Button> {' '}
-                        <Button type="submit"><b> Next {' ->'}</b> </Button>
-                    </Form>
-                </Row> */}
-            </div>
+                        <div className='col-sm-6'>
+                            <input className="form-control-static" type="text" value={user.address?.street ?? ''} readOnly />
+                        </div>
+                    </div>
 
+                    <div className='row'>
+                        <div className='col-sm-2'>
+                            <label className='displayInLine_label'> City: </label>
+                        </div>
+                        <div className='col-sm-6'>
+                            <input className="form-control-static" type="text" value={user.address?.city ?? ''} readOnly />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-sm-2'>
+                            <label className='displayInLine_last_label'> Zip Code: </label>
+                        </div>
+                        <div className='col-sm-6'>
+                            <input className="form-control-static" type="text" value={user.address?.zipcode ?? ''} readOnly />
+                        </div>
+                    </div>
+                </div >
+                <br />
+                <div className='row flex-row-reverse'>
+                    <div className='col-sm-2'>
+                        <Button variant="danger" onClick={(e) => callbackNavigate(e)}> Delete </Button>
+                    </div>
+                    <div className='col-sm-2'>
+                        <Button variant="info" onClick={(e) => callbackNavigate(e)}> Update </Button>
+                    </div>
+                </div>
+            </div >
+            <br />
         </>
     )
 }
